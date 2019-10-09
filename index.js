2619d201b8385209e4f116afc3c5b9496e943142
Chris Whitney <cwhitney@kollective.com>
2019-10-09 13:37:51 -0700

    Setup group scraping

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
-const puppeteer = require('puppeteer');
-const querystring = require('querystring');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-async function scrapePage(res) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const params = {
-    keywords: '',
-    radius: 30,
-    userFreeform: 'Bend%2C+OR',
-    mcId: 'z97701',
-    mcName: 'Bend%2C+OR',
-    month: 10,
-    day: 7,
-    year: 2019
-  };
-  params.allMeetups = !Boolean(params.keywords);
-  await page.goto(`https://www.meetup.com/find/events/?${querystring.stringify(params)}`);
-
-  // Get the "viewport" of the page, as reported by the page.
-  const listings = await page.evaluate(() => {
-    const results = [...document.querySelectorAll('.event-listing')];
-      return results.map(listing => {
-        const datetime = listing.querySelector('time');
-        const groupInfo = listing.querySelector('.text--labelSecondary a');
-        const eventInfo = listing.querySelector('.event');
-
-        return {
-          eventTitle: eventInfo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ''),
-          eventLink: eventInfo.href,
-          groupName: groupInfo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ''),
-          groupLink: groupInfo.href,
-          date: datetime.getAttribute('datetime'),
-        };
-      });
-  });
-  await browser.close();
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  console.log('listings:', listings);
-  res.end(JSON.stringify(listings));
-};
+const scrapePage = require('./src/events');
+const scrapeGroup = require('./src/group');
 
 app.use(express.static('public'));
 
@@ -50,12 +11,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// const server = http.createServer((req, res) => {
-//   console.log(req.url)
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'application/json');
-//   res.end('test');
-//   // scrapePage(res);
-// });
+app.get('/find', (req, res) => {
+  //?allMeetups=true&radius=50&userFreeform=Bend%2C+OR&mcId=z97701&mcName=Bend%2C+OR&sort=distance
+});
+
+app.get('/find/events', scrapePage);
+
+app.get('/:group', (req, res) => {
+  // const group = req.params.group;
+  scrapeGroup(req, res);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/events.js
@@ -0,0 +1,43 @@
+const puppeteer = require('puppeteer');
+const querystring = require('querystring');
+
+module.exports = async function scrapePage(req, res) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const currentDate = new Date();
+  const params = {
+    keywords: '',
+    radius: 30,
+    sort: 'distance',
+    userFreeform: 'Bend%2C+OR',
+    mcId: 'z97701',
+    mcName: 'Bend%2C+OR',
+    month: currentDate.getMonth() + 1,
+    day: currentDate.getDate(),
+    year: currentDate.getFullYear()
+  };
+  params.allMeetups = !Boolean(params.keywords);
+  await page.goto(`https://www.meetup.com/find/events/?${querystring.stringify(params)}`);
+
+  const listings = await page.evaluate(() => {
+    const results = [...document.querySelectorAll('.event-listing')];
+      return results.map(listing => {
+        const datetime = listing.querySelector('time');
+        const groupInfo = listing.querySelector('.text--labelSecondary a');
+        const eventInfo = listing.querySelector('.event');
+        const groupLink = groupInfo.href;
+        return {
+          eventTitle: eventInfo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ''),
+          eventLink: eventInfo.href,
+          groupName: groupInfo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ''),
+          groupLink,
+          self: groupLink.split('/').slice(-2, -1)[0],
+          date: datetime.getAttribute('datetime'),
+        };
+      });
+  });
+  await browser.close();
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(listings));
+};
