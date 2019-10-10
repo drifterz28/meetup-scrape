b9e26b7562c5be58868042f561dba3dd0f9900ce
Chris Whitney <cwhitney@kollective.com>
2019-10-09 22:30:01 -0700

    Add some dash

--- public/main.js
+++ public/main.js
@@ -6,6 +6,6 @@
   jsonElm.innerText = JSON.stringify(data, undefined, 2);
   data.map(group => {
     displayElm.insertAdjacentHTML('beforeend', `
-      <div class='listing'><a href='${group.groupLink}'>${group.groupName}</a><a href='/${group.next}'>View Group api</a></div>`);
+      <div class='listing'><a href='${group.groupLink}'>${group.groupName}</a> - <a href='/${group.next}'>View Group api</a></div>`);
   });
 })();
