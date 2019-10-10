f9983458814401786e1daf843d0feee10f953aeb
Chris Whitney <cwhitney@kollective.com>
2019-10-09 22:27:37 -0700

    Update api index view

---
+++ public/main.js
@@ -0,0 +1,11 @@
+'use strict';
+(async () => {
+  const data = await fetch('/find').then(res => res.json());
+  const jsonElm = document.getElementById('json-data');
+  const displayElm = document.getElementById('display-data');
+  jsonElm.innerText = JSON.stringify(data, undefined, 2);
+  data.map(group => {
+    displayElm.insertAdjacentHTML('beforeend', `
+      <div class='listing'><a href='${group.groupLink}'>${group.groupName}</a><a href='/${group.next}'>View Group api</a></div>`);
+  });
+})();
