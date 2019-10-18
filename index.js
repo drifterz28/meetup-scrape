f31eab8659e71f7de68c174a12b076b8f5e81e09
Chris Whitney <cwhitney@kollective.com>
2019-10-17 22:56:43 -0700

    Update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mcache = require('memory-cache');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const scrapePage = require('./src/events');
 const scrapeGroup = require('./src/group');
